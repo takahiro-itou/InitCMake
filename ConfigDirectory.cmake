15e94099dad8f5cd77b410251d13d8b6ee41a735
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-11-24 11:38:54 +0900

    リファクタリング
    (1) INCPKG_DIR_NAME -> incpkg_dir_name
    (2) INCLIB_SOURCE_DIR -> inclib_source_dir
    (3) INCLIB_BINARY_DIR -> inclib_binary_dir

--- ConfigDirectory.cmake
+++ ConfigDirectory.cmake
@@ -4,16 +4,16 @@
 ##    ディレクトリ名。
 ##
 
-set(INCPKG_DIR_NAME         "${PROJECT_NAME}")
-set(PACKAGE_INCLUDE_DIR     "${inctop_dir_name}/${INCPKG_DIR_NAME}")
+set(incpkg_dir_name         "${PROJECT_NAME}")
+set(PACKAGE_INCLUDE_DIR     "${inctop_dir_name}/${incpkg_dir_name}")
 
 set(INCTOP_SOURCE_DIR       "${PROJECT_SOURCE_DIR}/${inctop_dir_name}")
 set(INCTOP_BINARY_DIR       "${PROJECT_BINARY_DIR}/${inctop_dir_name}")
 set(LIBTOP_SOURCE_DIR       "${PROJECT_SOURCE_DIR}/${libtop_dir_name}")
 set(LIBTOP_BINARY_DIR       "${PROJECT_BINARY_DIR}/${libtop_dir_name}")
 
-set(INCLUDE_SOURCE_DIR      "${INCTOP_SOURCE_DIR}/${INCPKG_DIR_NAME}")
-set(INCLUDE_BINARY_DIR      "${INCTOP_BINARY_DIR}/${INCPKG_DIR_NAME}")
+set(INCLUDE_SOURCE_DIR      "${INCTOP_SOURCE_DIR}/${incpkg_dir_name}")
+set(INCLUDE_BINARY_DIR      "${INCTOP_BINARY_DIR}/${incpkg_dir_name}")
 
 
 ##----------------------------------------------------------------
