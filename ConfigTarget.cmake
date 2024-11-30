a55332ce93d3669930f6673fc1bdae5fddb6cfaa
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-11-30 13:01:36 +0900

    リファクタリング
    (1) ${PROJECT_NAME}-${LIBRARIES_TARGET_PREFIX} -> ${project_target_prefix}
    (2) ${PROJECT_NAME}- -> ${project_export_prefix}

---
+++ ConfigTarget.cmake
@@ -0,0 +1,10 @@
+
+##----------------------------------------------------------------
+##
+##    プロジェクト内のターゲットの参照
+##
+
+##  ターゲットのプレフィックス。
+
+set(project_target_prefix   "${PROJECT_NAME}-${LIBRARIES_TARGET_PREFIX}")
+set(project_export_prefix   "${PROJECT_NAME}-")
