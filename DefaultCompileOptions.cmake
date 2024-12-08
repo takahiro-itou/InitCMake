93f8f06e973e067fe611f81cecb3c924f91b7e38
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-12-08 19:29:07 +0900

    リファクタリング
    (1) CXX_WARNING_FLAGS -> cxx_warning_flags
    (2) C_WARNING_FLAGS -> c_warning_flags

--- DefaultCompileOptions.cmake
+++ DefaultCompileOptions.cmake
@@ -10,11 +10,11 @@ EndIf ()
 
 message("Build Type = ${CMAKE_BUILD_TYPE}")
 
-set(CXX_WARNING_FLAGS       "-Wall -Weffc++")
-set(C_WARNING_FLAGS         "-Wall")
+set(cxx_warning_flags       "-Wall -Weffc++")
+set(c_warning_flags         "-Wall")
 
-set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} ${CXX_WARNING_FLAGS}")
-set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS}   ${C_WARNING_FLAGS}")
+set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} ${cxx_warning_flags}")
+set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS}   ${c_warning_flags}")
 
 set(CMAKE_CXX_FLAGS_DEBUG       "${CMAKE_CXX_FLAGS_DEBUG} -D_DEBUG")
 set(CMAKE_C_FLAGS_DEBUG         "${CMAKE_C_FLAGS_DEBUG}   -D_DEBUG")
